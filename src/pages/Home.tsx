9046b41fb2ee273db48c67e8e4e2598fe37da3b8
zach-wearebattalion <zach@wearebattalion.com>
2021-04-03 21:22:22 +0200

    feat(Home): remove repos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent, useContext, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Container, Dropdown, Loader } from 'semantic-ui-react'
 import RepoGrid from '../components/RepoGrid';
 import RepoContext from '../contexts/RepoContext';
@@ -8,7 +8,7 @@ interface Props {
 }
 
 export const Home = (props: Props) => {
-    const { repos, loading, languages, filterByLanguage } = useContext(RepoContext);
+    const { loading, languages, filterByLanguage } = useContext(RepoContext);
     const [language, setLanguage] = useState<any>('');
     const handleLanguageFilter = (value: string | number | boolean | (string | number | boolean)[] | undefined) => {
         setLanguage(value);
